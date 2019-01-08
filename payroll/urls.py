9b1ba5a652cd1c05b5b54b14282d72eee30aa4ce
Oscar@DESKTOP-ML0AN6G <22925848+tyochan@users.noreply.github.com>
2019-01-08 16:52:26 +0800

    Fixed last payment pdf

--- payroll/urls.py
+++ payroll/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('last/ajax/payment_calculation', views.payment_calculation),
 
     path('payslipPDF/<int:pk>', views.PaymentPDFView.as_view(), name='payslip_pdf'),
+    path('last/payslipPDF/<int:pk>',
+         views.LastPaymentPDFView.as_view(), name='last_payslip_pdf'),
 ]
